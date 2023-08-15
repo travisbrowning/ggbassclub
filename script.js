b229cf72d810dc5a185a1497a622fceba6b5c0fa
travisbrowning <travisbrowning@me.com>
2023-08-14 22:31:48 -0500

    Cleans up code

---
+++ script.js
@@ -0,0 +1,9 @@
+let when = document.querySelector("#when");
+let where = document.querySelector("#where");
+let launch = document.querySelector("#launch");
+
+// Tournament Updates:
+
+when.textContent = "August 26th";
+where.textContent = "East River, Navarre";
+launch.textContent = "Hwy 87 Bridge";
